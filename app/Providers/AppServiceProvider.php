75f868706e13a6b458e222bb23b384bbe71c0167
waccoa <hominikal54@gmail.com>
2022-03-12 21:43:11 +0900

    change https develop

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,10 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191); /// 追加
-    }
-     //public function boot()
-   // {
         //
-   // }
+        if (\App::environment(['production']) || \App::environment(['develop'])){
+            \URL::forceScheme('https');
+        }
+    }
 }
